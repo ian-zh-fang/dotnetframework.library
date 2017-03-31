ac8d3437a338c3cf343a714ca0757ff3ce4d1394
ian.zh.fang <ian.zh.fang@outlook.com>
2017-03-31 17:38:49 +0800

    添加 unity IOC支持

---
+++ zh.fang.csharp.ioc.unity/UnityDependencyResolver.cs
@@ -0,0 +1,25 @@
+﻿namespace zh.fang.csharp.ioc.unity
+{
+    using System;
+    using Microsoft.Practices.Unity;
+
+    public class UnityDependencyResolver : DependencyResolver
+    {
+        public UnityDependencyResolver(IUnityContainer container)
+        {
+            Container = container;
+        }
+
+        protected override TInstance Instacne<TInstance>()
+        {
+            return Container.Resolve<TInstance>();
+        }
+
+        protected override object Instance(Type type)
+        {
+            return Container.Resolve(type);
+        }
+
+        public IUnityContainer Container { get; private set; }
+    }
+}
