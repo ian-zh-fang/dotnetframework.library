6ee45d2de38cc2e8703fd04388fed30d3875e218
振 方 <ian.zh.fang@gmail.com>
2017-04-17 11:07:59 +0800

    添加重复执行功能函数

---
+++ zh.fang.csharp/Func.cs
@@ -0,0 +1,37 @@
+﻿namespace zh.fang.csharp
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public static class FuncHelper
+    {
+        public static TResult Retry<TParam, TResult>(this Func<TParam, TResult> func, TParam parameter, Action<Exception> warn, int retryCount, int delayMilliseconds = 0)
+        {
+            int retry = 0;
+            TResult result = default(TResult);
+            while (retryCount > retry)
+            {
+                retry++;
+                try
+                {
+                    result = func.Invoke(parameter);
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    if (retryCount <= retry)
+                    {
+                        warn.Invoke(ex);
+                    }
+                }
+
+                if (delayMilliseconds > 0)
+                {
+                    Task.Delay(delayMilliseconds).Wait();
+                }
+            }
+
+            return result;
+        }
+    }
+}
